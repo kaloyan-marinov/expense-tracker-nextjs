5a19e6c2e49ed1efd1da07578a31b680d8bdaf44
root <happy-2025-04-06-14-33@protonmail.com>
2026-05-06 18:28:58 +0000

    create `lib/db.ts` as in the tutorial

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+/*
+Initialize a Prisma client,
+which makes it possible to interact with the database.
+
+This is a hack
+made necessary by the fact that Next.js has «hot reloading».
+This hack serves to
+prevent that «hot reloading» from initializing the `PrismaClient` over and over.
+
+*/
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
