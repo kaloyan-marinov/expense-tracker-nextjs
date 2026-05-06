dca2137a269bcfd7da450ddf32299e3b4790fcae
root <happy-2025-04-06-14-33@protonmail.com>
2026-05-06 18:33:06 +0000

    follow the recommendation within \app/generated/prisma/client.ts`

--- lib/db.ts
+++ lib/db.ts
@@ -9,12 +9,16 @@ prevent that «hot reloading» from initializing the `PrismaClient` over and ove
 
 */
 import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient({});
+const prismaClient = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+export const db = globalThis.prisma || prismaClient;
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
