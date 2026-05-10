3040ac3d9f40d52bb94c04c059b84e18bf615c02
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-10 20:21:09 +0200

    create a «server action» called `getTransaction` and make the `<TransactionList>` component call it

---
+++ actions/getTransactions.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { Transaction } from "@/types/Transaction";
+
+async function getTransactions(): Promise<{
+  transactions?: Transaction[];
+  error?: string;
+}> {
+  const { userId: clerkUserId } = await auth();
+
+  if (!clerkUserId) {
+    return {
+      error: "User not found",
+    };
+  }
+
+  try {
+    const transactions = await db.transaction.findMany({
+      where: {
+        userId: clerkUserId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return {
+      transactions,
+    };
+  } catch (error) {
+    return {
+      error: "Database error",
+    };
+  }
+}
+
+export default getTransactions;
--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,22 +1,12 @@
+import getTransactions from "@/actions/getTransactions";
 import { Transaction } from "@/types/Transaction";
 
-const TransactionList = () => {
-  const transactions = [
-    {
-      id: "id-1",
-      text: "text-1",
-      amount: 111,
-      userId: "userId-1",
-      createdAt: new Date(Date.now()),
-    },
-    {
-      id: "id-2",
-      text: "text-2",
-      amount: 222,
-      userId: "userId-2",
-      createdAt: new Date(Date.now()),
-    },
-  ];
+const TransactionList = async () => {
+  const { transactions, error } = await getTransactions();
+
+  if (error) {
+    return <p className="error">{error}</p>;
+  }
 
   return (
     <>
