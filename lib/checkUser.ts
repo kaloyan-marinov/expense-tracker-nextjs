ac3698ada046488a743628efaf799742c83fc472
root <happy-2025-04-06-14-33@protonmail.com>
2026-05-10 11:04:19 +0000

    add `lib/checkUser.ts`

---
+++ lib/checkUser.ts
@@ -0,0 +1,33 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export const checkUser = async () => {
+  // Obtain currently logged-in «Clerk user».
+  const clerkUser = await currentUser();
+
+  if (!clerkUser) {
+    return null;
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: clerkUser.id,
+    },
+  });
+
+  if (user) {
+    return user;
+  }
+
+  // Create a new user in the DB.
+  const newUser = await db.user.create({
+    data: {
+      clerkUserId: clerkUser.id,
+      name: `${clerkUser.firstName} {clerkUser.lastName}`,
+      imageUrl: clerkUser.imageUrl,
+      email: clerkUser.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newUser;
+};
