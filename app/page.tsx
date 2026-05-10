07e4925588c429a4f437af0194e096b5f08263b6
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-10 17:35:14 +0200

    add a static `<Balance>` component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Guest from "@/components/Guest";
 import { currentUser } from "@clerk/nextjs/server";
 import AddTransaction from "@/components/AddTransaction";
+import Balance from "@/components/Balance";
 
 const HomePage = async () => {
   const clerkUser = await currentUser();
@@ -11,7 +12,8 @@ const HomePage = async () => {
 
   return (
     <main>
-      <h1>Welcome, {clerkUser.firstName}</h1>
+      <h2>Welcome, {clerkUser.firstName}</h2>
+      <Balance />
       <AddTransaction />
     </main>
   );
